501db21df53887c04c59e2e1debe766caa8b1317
João Neves <joao95neves@gmail.com>
2019-10-20 21:14:12 +0100

    Update notes.js

--- notes.js
+++ notes.js
@@ -20,14 +20,21 @@
 /*
  * JS.MVC
  *
- * - Pages
- * - Components
- * 
+ * - Startup (to inject pages and do other stuff)
+ *   - Pages (Made of components)
+ *     - Components
+ *
+ * - Components (stand alone)
+ *   - Templates
+ *   - Model (store data)
+ *   - Events
+ *
  */
 
 /**
  * References:
- * 
+ *
  * - https://developer.mozilla.org/en-US/docs/Web/Web_Components
+ * - https://webdesign.tutsplus.com/articles/how-to-create-your-own-html-elements-with-web-components--cms-21524
  * - https://www.webcomponents.org/introduction
  */
