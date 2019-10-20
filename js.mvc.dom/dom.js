54a17786907e84bffc02b9d103884a7a441f5cbb
João Neves <joao95neves@gmail.com>
2019-10-20 18:58:06 +0100

    Added js.mvc.dom

---
+++ js.mvc.dom/dom.js
@@ -0,0 +1,65 @@
+﻿
+
+class Dom {
+
+  /**
+   * 
+   * @param { string } query
+   */
+  static get( query ) {
+    return document.querySelectorAll( query );
+  }
+
+  /**
+   * 
+   * @param { string } id
+   */
+  static getById( id ) {
+    return document.getElementById( id );
+  }
+
+  /**
+   * 
+   * @param { string } className
+   */
+  static getByClass( className ) {
+    return document.getElementsByClassName( className );
+  }
+
+  /**
+   * 
+   * @param { string } className
+   */
+  static getByClassFirst( className ) {
+    return Dom.getByClass( className )[0];
+  }
+
+  /**
+   * 
+   * @param { HTMLElementTagNameMap } tagName
+   */
+  static getByTag( tagName ) {
+    return document.getElementsByTagName( tagName );
+  }
+
+  /**
+   * 
+   * @param { string } tagName
+   */
+  static getByTagFirst( tagName ) {
+    return Dom.getByTag( tagName )[0];
+  }
+
+  /**
+   * 
+   * @param { string } event
+   * @param { HTMLElement } elem
+   * @param { Function } eventHandler
+   */
+  static on( event, elem, eventHandler ) {
+    elem.addEventListener( event, ( e ) => {
+      return eventHandler( e );
+    } );
+  }
+
+}
