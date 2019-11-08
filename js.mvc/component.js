367a7e6051149fec1b82d8aa78088782aea411e0
João Neves <joao95neves@gmail.com>
2019-11-08 23:26:58 +0000

    Added the Component class

--- js.mvc/component.js
+++ js.mvc/component.js
@@ -1,3 +1,23 @@
 ﻿
 class Component {
+
+  /**
+   * Events:
+   * <void> onInit()
+   * <void> onDestroy()
+   * 
+   * @param { string } targetElement The name of the element where the element should be appended.
+   * @param { string } templatePath The path of the template.
+   * @param { string[] } stylePaths An array of stylesheet file paths to inject on the component.
+   */
+  constructor( targetElement, templatePath, stylePaths ) {
+
+    this.targetElement = targetElement;
+
+    this.templatePath = templatePath;
+
+    this.stylePaths = stylePaths;
+
+  }
+
 }
