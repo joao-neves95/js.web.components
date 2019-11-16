63bad4cf783eb6f09626966e2f70d8446e0f5d48
João Neves <joao95neves@gmail.com>
2019-11-16 19:11:08 +0000

    Added compiled code examples for future reference (can change)

---
+++ js.web.components.exampleWebApp/header/header.template.js
@@ -0,0 +1,14 @@
+﻿
+const headerTemplate = `
+<div class="container">
+    <h1> <_r> title </_r> </h1>
+</div>
+`;
+
+// copiled code reference:
+// no template required.
+`
+<div class="container">
+    <h1> My Website </h1>
+</div>
+`;
--- js.web.components.exampleWebApp/nameList/nameList.template.js
+++ js.web.components.exampleWebApp/nameList/nameList.template.js
@@ -8,3 +8,21 @@ const nameListTemplate = `
     </ul>
 </div>
 `;
+
+// compiled code reference:
+`
+<div class="container">
+  <ul app-nameList_for_1>
+    <li> John Doe </li>
+    <li> Oliver Hoe </li>
+    <li> Fiona Silva </li>
+  </ul>
+</div>
+`;
+
+// compiled template reference:
+`
+<template target="app-nameList_for_1" component="app-nameList" binding="names" type="for">
+  <li> <_r>name</_r> </li>
+</template>
+`;
