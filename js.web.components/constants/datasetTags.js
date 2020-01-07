aa4e77e60b9564afc5e21494692601e57606ca98
João Neves <joao95neves@gmail.com>
2020-01-07 15:31:52 +0000

    [components/constants/]: Add the dataset tags to avoid hardcoded values.

---
+++ js.web.components/constants/datasetTags.js
@@ -0,0 +1,13 @@
+
+const DATA_SET_PREFIX = 'data-';
+
+const DATA_SET_TAGS = Object.freeze({
+  Component: 'component',
+  Component_Prefixed: DATA_SET_PREFIX + 'component',
+  BindingTo: 'binding',
+  BindingTo_Prefixed: DATA_SET_PREFIX + 'binding',
+  EventMethodCall: 'eventmethodcall',
+  EventMethodCall_Prefixed: DATA_SET_PREFIX + 'eventmethodcall',
+  EventMethodToCall: 'eventmethodname',
+  EventMethodToCall_Prefixed: DATA_SET_PREFIX + 'eventmethodname'
+});
