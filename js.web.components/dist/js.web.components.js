bf714821746f0ae6ec791e39c01bf6e1295ca440
João Neves <joao95neves@gmail.com>
2020-01-13 20:10:47 +0000

    [ exampleWebApp/ ]: Remove test code.

--- js.web.components/dist/js.web.components.js
+++ js.web.components/dist/js.web.components.js
@@ -618,7 +618,7 @@ class MethodCallOnEvent {
 }
 
 /*
- * Copyright (c) 2019 Jo�o Pedro Martins Neves (shivayl) - All Rights Reserved.
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
  *
  * All content is licensed under the GNU Lesser General Public License (LGPL),
  * version 3, located in the root of this project, under the name "LICENSE.md".
@@ -1055,7 +1055,7 @@ This is most likely a syntax error on the event property. The method call token 
 }
 
 /*
- * Copyright (c) 2019 Jo�o Pedro Martins Neves (shivayl) - All Rights Reserved.
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
  *
  * All content is licensed under the GNU Lesser General Public License (LGPL),
  * version 3, located in the root of this project, under the name "LICENSE.md".
