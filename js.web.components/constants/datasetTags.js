50e35a35f1a9c169bf06bc58a19f1c729b8f7b1d
João Neves <joao95neves@gmail.com>
2020-01-15 15:39:49 +0000

    [ components/constants/ ]: Refactored DATA_SET_TAGS to avoid repetition.
    DRY

--- js.web.components/constants/datasetTags.js
+++ js.web.components/constants/datasetTags.js
@@ -2,12 +2,14 @@
 const DATA_SET_PREFIX = 'data-';
 
 const DATA_SET_TAGS = Object.freeze({
+  ComponentId: 'componentid',
+  ComponentId_Prefixed: DATA_SET_PREFIX + DATA_SET_TAGS.ComponentId,
   Component: 'component',
-  Component_Prefixed: DATA_SET_PREFIX + 'component',
+  Component_Prefixed: DATA_SET_PREFIX + DATA_SET_TAGS.Component,
   BindingTo: 'binding',
-  BindingTo_Prefixed: DATA_SET_PREFIX + 'binding',
+  BindingTo_Prefixed: DATA_SET_PREFIX + DATA_SET_TAGS.BindingTo,
   EventMethodCall: 'eventmethodcall',
-  EventMethodCall_Prefixed: DATA_SET_PREFIX + 'eventmethodcall',
+  EventMethodCall_Prefixed: DATA_SET_PREFIX + DATA_SET_TAGS.EventMethodCall,
   EventMethodToCall: 'eventmethodname',
-  EventMethodToCall_Prefixed: DATA_SET_PREFIX + 'eventmethodname'
+  EventMethodToCall_Prefixed: DATA_SET_PREFIX + DATA_SET_TAGS.EventMethodToCall
 });
